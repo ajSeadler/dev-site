e3a84e1599a4129b76159f721b5b92102d1fd34f
ajSeadler <aj6287@gmail.com>
2024-09-30 10:26:17 -0500

    styling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,9 @@ import "../styles/Projects.css"; // Ensure to create this CSS file
 function Projects() {
   return (
     <div className="projects-section">
-      <h1 className="main-title">My Projects</h1>
+      <div className="main-title-container">
+    <h1 className="main-title">Projects</h1>
+  </div>
       <div className="project-info-card">
         <h2 className="project-title">Circle of Fifths Viewer</h2>
         <p className="project-description">
@@ -14,7 +16,7 @@ function Projects() {
 
       </div>
       <div className="image-container-projects">
-        {/* First Project */}
+        {/* First Image */}
         <div className="project-wrapper">
           <div className="project-image">
             <img src="/2.png" alt="Home Page" />
@@ -27,12 +29,12 @@ function Projects() {
           </div>
         </div>
 
-        {/* Second Project */}
+        {/* Second Image */}
         <div className="project-wrapper">
           <div className="project-image">
             <img src="/3.png" alt="Scale Selection" />
           </div>
-          <div className="text-container">
+          <div className="text-container-projects">
             <h3 className="project-subtitle">Circle of Fifths & Scale Selection</h3>
             <p className="project-text">
             Users can interact with the Circle of Fifths chart to select a key and corresponding scale, providing an educational tool for exploring music theory.
@@ -40,12 +42,12 @@ function Projects() {
           </div>
         </div>
 
-        {/* Third Project */}
+        {/* Third Image */}
         <div className="project-wrapper">
           <div className="project-image">
             <img src="/4.png" alt="Scale Display" />
           </div>
-          <div className="text-container">
+          <div className="text-container-projects">
             <h3 className="project-subtitle">Chord & Interval Display</h3>
             <p className="project-text">
             Based on the selected key and scale, the app displays chords and intervals, giving users a detailed breakdown of musical components.
